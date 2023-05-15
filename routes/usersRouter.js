bbd05ade34d7f9b7c5d39f19beb4bc2f7ca96556
Brian Stricker <brianmstricker@gmail.com>
2023-05-15 14:06:24 -0400

    cors

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,8 +3,10 @@ const User = require("../models/user");
 const router = express.Router();
 const passport = require("passport");
 const authenticate = require("../authenticate");
+const cors = require("./cors");
 router.get(
   "/",
+  cors.corsWithOptions,
   authenticate.verifyUser,
   authenticate.verifyAdmin,
   function (req, res, next) {
@@ -17,7 +19,7 @@ router.get(
       .catch((err) => next(err));
   }
 );
-router.post("/signup", (req, res) => {
+router.post("/signup", cors.corsWithOptions, (req, res) => {
   User.register(
     new User({ username: req.body.username }),
     req.body.password,
@@ -50,17 +52,22 @@ router.post("/signup", (req, res) => {
     }
   );
 });
-router.post("/login", passport.authenticate("local"), (req, res) => {
-  const token = authenticate.getToken({ _id: req.user._id });
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json({
-    success: true,
-    token: token,
-    status: "You are successfully logged in!",
-  });
-});
-router.get("/logout", (req, res, next) => {
+router.post(
+  "/login",
+  cors.corsWithOptions,
+  passport.authenticate("local"),
+  (req, res) => {
+    const token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "You are successfully logged in!",
+    });
+  }
+);
+router.get("/logout", cors.corsWithOptions, (req, res, next) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie("session-id");
